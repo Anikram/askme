98b8062745b507b2d6bab37a5ecce6e1e4bee10b
Anikram <4nikram@gmail.com>
2018-04-23 15:26:47 +0300

    Lesson50 - stylesheets, Views refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require_tree .
+
+$(function(){
+    $('#ask-button').click(function(){
+        $('#ask-form').slideToggle(300);
+        return false;
+    });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,38 @@
-# Регистрация, авторизация пользователей
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+            id: 1,
+            name: 'Masha',
+            username: 'attarochka'
+      ),
+      User.new(
+            id: 2,
+            name: 'Sasha',
+            username: 'Zuluzs'
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет, Маша!"
+    @user = User.new(
+                  name: 'Alexander',
+                  username: 'anikram',
+                  avatar_url: 'https://ru.gravatar.com/avatar/a4ea88e2f841603eaed3ea528a834a42'
+
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2017')),
+      Question.new(text: 'Какой ваш любимый цвет?', created_at: Date.parse('27.03.2017'))
+    ]
+
+    @new_question = Question.new
   end
 end
