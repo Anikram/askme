1e71fd715f15fe220c3517b435bfeb24eb4e8c2e
Anikram <4nikram@gmail.com>
2018-04-24 15:39:47 +0300

    decline_word helper moved to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,26 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def decline_word (number, krokodil, krokodila, krokodilov) #lesson50 - decline method for question counter
+    if (number == nil || !number.is_a?(Numeric))
+      number = 0
+    end
+
+    remainder = number % 100
+
+    if (remainder >= 11 && remainder <= 20)
+      return krokodilov
+    end
+
+    remainder = number % 10
+
+    if (remainder == 1)
+      return  krokodil
+    elsif (remainder >= 2 && remainder <= 4)
+      return  krokodila
+    else
+      return krokodilov
+    end
+  end
 end
