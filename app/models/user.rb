68c45efdbb13d9f180a4fc3d0413516aa54316cf
Anikram <4nikram@gmail.com>
2018-04-24 14:52:26 +0300

    Lesson50-1 decline_it Helper added & gitignore upadted

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,29 +54,4 @@ class User < ApplicationRecord
       nil
     end
   end
-
-  def decline_word (number, krokodil, krokodila, krokodilov) #lesson50 - decline method for question counter
-    if (number == nil || !number.is_a?(Numeric))
-      number = 0
-    end
-
-    remainder = number % 100
-
-    if (remainder >= 11 && remainder <= 20)
-      return krokodilov
-    end
-
-    remainder = number % 10
-
-    if (remainder == 1)
-      return  krokodil
-    elsif (remainder >= 2 && remainder <= 4)
-      return  krokodila
-    else
-      return krokodilov
-    end
-  end
-
-
-
 end
